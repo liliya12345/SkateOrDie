4267ab86fee678e7786dae23daba8f515b4c0a40
windnorthern <qwert_ettyu@mail.ru>
2025-12-30 18:06:04 +0100

    fetch

--- js/script.js
+++ js/script.js
@@ -85,13 +85,14 @@ function setupSmoothScrolling() {
   });
 }
 
-// Helper Functions
+
 function darkenColor(color, percent) {
   const num = parseInt(color.replace("#", ""), 16);
   const amt = Math.round(2.55 * percent);
   const R = (num >> 16) - amt;
   const G = (num >> 8 & 0x00FF) - amt;
   const B = (num & 0x0000FF) - amt;
+  console.log("")
   return "#" + (0x1000000 + (R < 255 ? R < 1 ? 0 : R : 255) * 0x10000 +
     (G < 255 ? G < 1 ? 0 : G : 255) * 0x100 +
     (B < 255 ? B < 1 ? 0 : B : 255)).toString(16).slice(1);
